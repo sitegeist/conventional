07b139df4179c3c8b759dc12ec4a8fbbd1fdd31b
Lukas Gurschler <gurschler@sitegeist.de>
2020-03-20 11:47:20 +0100

    fix prefix json write

--- bin/commands/build.js
+++ bin/commands/build.js
@@ -1,5 +1,5 @@
 const conventional = require('../../lib/conventional');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 
 module.exports = function (command, workingDir, config) {
     conventional.javascript.build(workingDir, config);
@@ -12,22 +12,22 @@ module.exports = function (command, workingDir, config) {
                 config.minify,
                 config.sourceMap,
                 config.prefixSalt
-            ).then(() => {
+            ).then((buildSuccessMessage) => {
                 // Write Prefix Json only on first css loop
                 if(!index) {
-
                     // get prefix data from global array defined in the prefix importer
                     const prefixData = global.prefixData;
-
-                    fs.writeFile(config.prefixOutputFile, JSON.stringify(prefixData), (writeFileErr) => {
-                        if (writeFileErr) {
-                            console.log(new Error(writeFileErr));
-                        }
-                        console.log(`${config.prefixOutputFile} created`);
-                    });
+                    fsPromises.writeFile(config.prefixOutputFile, JSON.stringify(prefixData))
+                        .then(() => {
+                            console.log(`${config.prefixOutputFile} created`);
+                        })
+                        .catch(er => {
+                            console.log(er);
+                        });
                 }
+                console.log(buildSuccessMessage);
             })
-            .catch(data => ( console.log(data)  ));
+            .catch(buildErrorMessage => (console.log(buildErrorMessage)));
     })
 
 };
